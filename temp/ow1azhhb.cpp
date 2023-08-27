00683100e96120108b13150f6b36da3ca531fa5b
Priya Chauhan <Priya@123>
2023-08-27 13:49:17 +0530

    added

---
+++ temp/ow1azhhb.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+  int A,B,sum;
+  cin>>A>>B;
+  
+	// Given two number
+	sum=A+B;
+
+	// Function call
+	cout << "sum = " << sum;
+	return 0;
+}
+
